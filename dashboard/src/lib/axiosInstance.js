c6cba90562d2590ae32b80b9ec2b75de6f384b19
Deepak Kushwaha <128895460+Codebuster0001@users.noreply.github.com>
2025-07-02 10:43:55 +0530

    update the change

--- dashboard/src/lib/axiosInstance.js
+++ dashboard/src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL_DASHBOARD,
+  baseURL: import.meta.env.VITE_API_URL_DASHBOARD || "http://localhost:5000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
